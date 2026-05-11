68d3571cabaa9d7a1c51c383d98832b29bc263b8
BenHorrisberger <benhorrisberger@gmail.com>
2026-05-11 13:11:04 -0700

    configured markview split preview keymap

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -14,4 +14,5 @@ return {
     end,
 
     vim.api.nvim_set_keymap("n", "<leader>m", "<CMD>Markview<CR>", { desc = "Toggles `markview` previews globally." });
+    vim.api.nvim_set_keymap("n", "<leader>ms", "<CMD>Markview splitToggle<CR>", { desc = "Toggles `splitview` for current buffer." });
 };
